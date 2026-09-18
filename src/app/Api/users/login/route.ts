fb7c26ade53f49ad29af1a43e9bc58efb3c553c8
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-09-18 23:38:12 +0530

    wroked on login validations

---
+++ src/app/Api/users/login/route.ts
@@ -0,0 +1,18 @@
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModels";
+import {NextRequest, NextResponse} from "next/server"
+import bcryptjs from "bcryptjs";
+
+
+
+connect()
+
+export async function POST(request: NextRequest){
+    try {
+        
+    } catch (error: any) {
+        return NextRequest.json({error: error.message},
+            {status: 500}
+        )
+    }
+}
