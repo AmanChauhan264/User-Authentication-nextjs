acc28e9a70ad4c24e1fde370f8b181e1716490f3
AmanChauhan264 <neerajchauhanch112@gmail.com>
2026-09-19 10:24:42 +0530

    worked on middleware and me route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,27 @@
-import {NextResponse} from 'next/server';
-import type {NextResponse} from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest){
-   
+   const path = request.nextUrl.pathname
+
+   const isPublicPath = path === '/login' || path === 'signup'
+
+   const token = request.cookies.get('token')?.value || ''
+
+   if(isPublicPath && token){
+    return NextResponse.redirect(new URL('/', request.nextUrl))
+   }
+
+   if(!isPublicPath && !token){
+    return NextResponse.redirect(new URL('/login', request.nextUrl))
+   }
+
 }
 
 export const config = {
-    matcher: {
+    matcher: [
         '/',
         '/profile',
         '/login',
         '/signup',
-    }
+],
 }
